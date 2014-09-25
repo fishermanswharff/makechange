3460e931d0a6c400547aa6730803951c80ae781e
fishermanswharff <fishermanswharff@github.com>
2014-09-24 20:34:39 -0400

    comments to MakeChange.rb, README.md with instructions

--- MakeChange.rb
+++ MakeChange.rb
@@ -1,17 +1,28 @@
 def makeChange(price, paid)
 
-  # calculate the difference,
+  # calculate the difference, multiply it by 100 to make things easy on ourselves
   diff = (100*(paid-price)).round(2)
+
+  # these are the denominations that we have to keep in our register. 1000 = $10.00, 500 = $5.00, so on and so forth
   coins = [1000,500,100,25,10,5,1]
+
+  # create the array that will hold our return denominations
   change = []
+
+  # your logic
   coins.each { |coin|
   	change << (diff/coin).floor
   	diff -= ((diff/coin).floor)*coin
   }
-  puts change
+
+  # this array represents the amount of each denomination to return as change
+  print change
 end
 
-makeChange(17.50,20.00)
+
+# return the change, you can pass in anything here. First parameter is the price, second parameter is what was paid
+makeChange(17.28,20.00)
+
 
 
 =begin makeChange v.1
@@ -36,6 +47,3 @@ makeChange(17.50,20.00)
 	# diff -= (p*1)
 	# puts diff
 =end
-
-# challenge: write a letter, track those changes through github
-# git real
